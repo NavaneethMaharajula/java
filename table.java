8b8dffaed8b8a3600f290dcf5eb7f46c387754a9
NavaneethMaharajula <120824716+NavaneethMaharajula@users.noreply.github.com>
2024-01-30 21:32:34 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+public class DisplayTable
+{
+	public static void main(String[] args) 
+	{
+	JFrame f = new JFrame("FILE READING EXAMPLE");
+		try 
+		{
+		Scanner st=new Scanner(new FileReader("Emp.txt"));
+                        st.useDelimiter("\\s*,\\s*");
+			while(st.hasNext())
+			{
+				f.add(new Label(st.next()));
+			f.setLayout(new GridLayout(6,2));
+				f.setSize(400,200);
+				f.setVisible(true);
+			}
+		} 
+		 catch (Exception ex)
+		{
+			System.out.println("Error reading file ");
+		} 
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
